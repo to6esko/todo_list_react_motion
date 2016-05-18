325f42b63653af9d309fff1ad9d83c49199b5d55
Todor Krastev <to6esko69@gmail.com>
2016-05-18 17:51:12 +0300

    run animaiton

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
 import React from 'react';
-class App extends React.Component {
-    render(){
-        return <div>Hello</div> 
+import {Motion, spring} from 'react-motion';
+import presets from './presets';
+
+
+
+class App extends React.Component{
+    constructor() {
+        super();
     }
+    
+     render() {
+    return(
+      <Motion defaultStyle={{y: 500}} style={{y: spring(100,presets.woblly)}}>
+      {(obj) =>{
+          const {y}=obj;
+          let style={
+              WebkitTransform: `translate3d(0, ${y}px, 0)`,
+                  transform: `translate3d(0, ${y}px, 0)`
+          }
+          return (
+                <div style={style} className='block'>
+                  
+                  </div>
+          )
+      }}
+      </Motion>
+    )
+  }
 }
+
 export default App;
