a9f83d26c19e0456821e28cc59615b00e1cb346d
Todor Krastev <to6esko69@gmail.com>
2016-05-18 18:56:51 +0300

    add moble in presets

--- App.js
+++ App.js
@@ -11,17 +11,17 @@ class App extends React.Component{
     
      render() {
     return(
-      <Motion defaultStyle={{y: 500}} style={{y: spring(100,presets.woblly)}}>
+      <Motion 
+      defaultStyle={{y: 500,z:10}} 
+      style={{y: spring(100,presets.moble),z:spring(2)}}>
       {(obj) =>{
-          const {y}=obj;
+          const {y,z}=obj;
           let style={
-              WebkitTransform: `translate3d(0, ${y}px, 0)`,
-                  transform: `translate3d(0, ${y}px, 0)`
+              WebkitTransform: `translate3d(100px, ${y}px,0) scale(${z}) `,
+                  transform: `translate3d(100px, ${y}px,0) scale(${z}) `
           }
           return (
-                <div style={style} className='block'>
-                  
-                  </div>
+                <div style={style} className='block'></div>
           )
       }}
       </Motion>
--- presets.js
+++ presets.js
@@ -3,4 +3,5 @@ export default {
   gentle: {stiffness: 120, damping: 14},
   wobbly: {stiffness: 180, damping: 12},
   stiff: {stiffness: 210, damping: 20},
+  moble:{stiffness:100,damping:40},
 };
